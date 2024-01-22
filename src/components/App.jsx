f4eeaaad0b4fbf1f44dd80cdc7afc55d707b7928
ValentynHromadskyi <126354727+ValentynHromadskyi@users.noreply.github.com>
2024-01-22 20:27:05 +0200

    update hw02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,51 @@ import "./App.css";
 import { Description } from "./Descripton/Description";
 import { Feedback } from "./Feedback/Feedback";
 import { Optins } from "./Options/Options";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [feedback, setFeedback] = useState(
-    JSON.parse(window.localStorage.getItem("feedback")) || {
+const getInitialClicks = () => {
+  const savedFeedback = window.localStorage.getItem("feedback");
+  return (
+    JSON.parse(savedFeedback) || {
       good: 0,
       neutral: 0,
       bad: 0,
     }
   );
-  const handleClick = () => {
-    setFeedback(JSON.parse(window.localStorage.getItem("feedback")));
+};
+
+function App() {
+  const [feedback, setFeedback] = useState(getInitialClicks);
+  const handleClick = (option) => {
+    if (option === "Reset") {
+      setFeedback({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      });
+      return;
+    }
+    setFeedback((preFeedbacks) => {
+      return {
+        ...preFeedbacks,
+        [option]: preFeedbacks[option] + 1,
+      };
+    });
   };
 
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  const positive = Math.round(
+    ((feedback.good + feedback.neutral) / total) * 100
+  );
   return (
     <>
       <Description />
-      <Optins onUpdate={handleClick} />
-      <Feedback value={feedback} />
+      <Optins onUpdate={handleClick} total={total} />
+      <Feedback feedback={feedback} total={total} positive={positive} />
     </>
   );
 }
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,19 +1,18 @@
 import css from "./Feedback.module.css";
 
-export const Feedback = ({ value: { good, neutral, bad } }) => {
-  if (good === 0 && neutral === 0 && bad === 0) {
-    return <h2>No feedback yet</h2>;
-  }
+export const Feedback = ({
+  feedback: { good, neutral, bad },
+  total,
+  positive,
+}) => {
+  if (!total) return <h2>No feedback yet</h2>;
   return (
     <ul className={css.feedback}>
-      <li>Good {good || 0}</li>
-      <li>Neutral {neutral || 0}</li>
-      <li>Bad {bad || 0}</li>
-      <li>Total {(good || 0) + (neutral || 0) + (bad || 0)}</li>
-      <li>
-        Positive
-        {Math.round(((good + neutral) / (good + neutral + bad)) * 100) || 0}%
-      </li>
+      <li>Good {good}</li>
+      <li>Neutral {neutral}</li>
+      <li>Bad {bad}</li>
+      <li>Total {total}</li>
+      <li>Positive {Math.round(positive)} %</li>
     </ul>
   );
 };
